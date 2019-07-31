ff5633f8c0a546ec3119832d184106692ecec5f7
TorquayAcademy <40391350+TorquayAcademy@users.noreply.github.com>
2019-07-31 10:21:01 +0100

    Add files via upload

--- scripts/presentation.js
+++ scripts/presentation.js
@@ -23,10 +23,31 @@ var tbLetter = document.getElementById("letter");
 tbLetter.style.width = Math.floor(tbLetter.offsetHeight) + 'px';
 });
 
-/*var tbHeader = document.getElementById("header");
-tbHeader.style.width = Math.floor(tbHeader.offsetHeight) * 6.79333333 + 'px';
-	
-window.addEventListener("resize", function() {
-var tbHeader = document.getElementById("header");
-tbHeader.style.width = Math.floor(tbHeader.offsetHeight) * 6.79333333 + 'px';
-});*/
+function GetDate(){
+var tday=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var tmonth=["January","February","March","April","May","June","July","August","September","October","November","December"];
+var d=new Date();
+var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
+
+var dateText=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+"";
+document.getElementById('dateBox').innerHTML=dateText;
+}
+
+
+function GetTime(){
+var d=new Date();
+var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds(),ap;
+
+if(nhour==0){ap=" AM";nhour=12;}
+else if(nhour<12){ap=" AM";}
+else if(nhour==12){ap=" PM";}
+else if(nhour>12){ap=" PM";nhour-=12;}
+
+if(nmin<=9) nmin="0"+nmin;
+if(nsec<=9) nsec="0"+nsec;
+
+var timeText=""+nhour+":"+nmin+":"+nsec+ap+"";
+document.getElementById('timeBox').innerHTML=timeText;
+}
+
+
